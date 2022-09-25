bbab5bd4fb2aa772efbc1b205a7a538c45910d92
Thibaut Maréchal <t.marechal@evs.com>
2022-09-25 17:27:29 +0200

    done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-const InitalDate = new Date('2022-09-26').getTime();
+const InitalDate = new Date('2022-08-29');
 
-function getWeeksDiff(startDate: number, endDate: number) {
+function getMondayOfCurrentWeek(date: Date) {
+  const dayOfMonth = date.getDate();
+  const dayOfWeek = (date.getDay() + 6) % 7
+  const dayOfMonthOfMondayThisWeek = dayOfMonth - dayOfWeek ;
+  return new Date(new Date(date).setDate(dayOfMonthOfMondayThisWeek));
+}
+
+function getWeeksDiff(startDate:Date, endDate: Date) {
   const msInWeek = 1000 * 60 * 60 * 24 * 7;
-
-  return Math.round(Math.abs(endDate - startDate) / msInWeek);
+  return Math.round(Math.abs(endDate.getTime() - startDate.getTime()) / msInWeek);
 }
 
 const Home: NextPage = () => {
   const persons = ['Noémie', 'Thibaut', 'Kim', 'Alissia'];
-  const today = Date.now()
-  console.log()
+  const today = new Date(Date.now());
+  const currentIndex = getWeeksDiff(getMondayOfCurrentWeek(InitalDate), getMondayOfCurrentWeek(today));
+  const nextIndex = currentIndex + 1
   return (
     <>
       <Head>
@@ -20,12 +27,48 @@ const Home: NextPage = () => {
         <meta name="description" content="Parking calendar" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-          {persons[getWeeksDiff(InitalDate, today) % persons.length]}
-        </h1>
-      </main>
+      <div className="bg-blue-200 w-screen h-screen p-10">
+        <div className="bg-blue-500 text-white border-white border-8 sm:border-[14px] md:border-[20px] rounded-xl aspect-[2/3] max-w-full max-h-full m-auto">
+          <svg viewBox="0 0 200 300" xmlns="http://www.w3.org/2000/svg">
+            <text
+            x={100}
+            y={150}
+            fontSize={250}
+            textAnchor="middle"
+            dominantBaseline="middle"
+            style={{
+              fill: 'white',
+              fontWeight: 'bolder',
+            }}>
+              P
+            </text>
+            <text
+              x={100}
+              y={275}
+              fontSize={35}
+              textAnchor="middle"
+              dominantBaseline="middle"
+              style={{
+                fill: 'white',
+                fontWeight: 'bolder',
+              }}>
+              {persons[currentIndex % persons.length]}
+            </text>
+            <text
+              x={100}
+              y={295}
+              fontSize={5}
+              textAnchor="middle"
+              dominantBaseline="middle"
+              style={{
+                fill: 'white',
+                fontWeight: 'bolder',
+              }}>
+              (après: {persons[nextIndex % persons.length]})
+            </text>
+          </svg>
+        </div>
+      </div>
     </>
   );
 };
