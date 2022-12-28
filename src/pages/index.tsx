8d82204089ee9b8bea3224f5c88f7dd5aa3a7a41
Thibaut Maréchal <t.marechal@evs.com>
2022-12-28 15:22:10 +0100

    add Autumn leaves

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,43 @@ import { Fireworks } from '@fireworks-js/react'
 
 function isWinter(date: Date){
   const month =  date.getMonth()
-  return month === 11 || month <= 2
+  return month === 11 || month <= 2;
+}
+function isAutumn(date: Date){
+  const month =  date.getMonth()
+  return month >= 10 && month < 11;
 }
 
 function isNewYear(date: Date) {
   const month =  date.getMonth()
   const day =  date.getDate()
   return month === 11 && day === 31 || month === 0 && day === 1
+}
+
+function getSpecialDatePerson(date: Date){
+  const month = date.getMonth();
+  const day = date.getDate();
+
+  if(month === 3 && day === 1) {
+    return 'Nesta'
+  }
+  if(month === 11 && day === 25) {
+    return 'Jesus'
+  }
+  if(month === 7 && day === 15) {
+    return 'Tchantchès'
+  }
+}
+
+function getAutumunLeaveImages() {
+  if(typeof document !== 'undefined'){
+    const leaf1 = document.createElement('img');
+    leaf1.src = '/autumn-leaf-1.png';
+    const leaf2 = document.createElement('img');
+    leaf2.src = '/autumn-leaf-2.png';
+    return [leaf1, leaf2]
+  }
+  return []
 }
 
 const InitalDate = new Date('2022-08-29');
@@ -31,8 +61,10 @@ function getWeeksDiff(startDate:Date, endDate: Date) {
 const Home: NextPage = () => {
   const persons = ['Noémie', 'Thibaut', 'Kim', 'Alissia'];
   const today = new Date(Date.now());
+  // const today = new Date(2022, 10, 1);
   const currentIndex = getWeeksDiff(getMondayOfCurrentWeek(InitalDate), getMondayOfCurrentWeek(today));
   const nextIndex = currentIndex + 1;
+
   return (
     <>
       <Head>
@@ -58,7 +90,11 @@ const Home: NextPage = () => {
         />)
       }
       <div className="bg-blue-500 text-white border-white border-8 transition-all sm:border-[20px] rounded-3xl aspect-[2/3] m-auto inset-0 fixed max-h-[75vh] max-w-[75vw]">
-        {isWinter(today) && <Snowfall />}
+        {isWinter(today) ?
+          <Snowfall />
+        : isAutumn(today) ?
+          <Snowfall snowflakeCount={25} images={getAutumunLeaveImages()} radius={[25, 50]}/>
+        : null}
         <svg viewBox="0 0 200 300" xmlns="http://www.w3.org/2000/svg">
           <text
           x={100}
@@ -82,7 +118,7 @@ const Home: NextPage = () => {
               fill: 'white',
               fontWeight: 'bolder',
             }}>
-            {persons[currentIndex % persons.length]}
+            {getSpecialDatePerson(today) || persons[currentIndex % persons.length]}
           </text>
           <text
             x={100}
